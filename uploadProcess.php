e35f9b8cdb69ce667ffd6810e176f16eaf428fc1
Eric Boehs <ericboehs@gmail.com>
2008-10-28 12:07:27 -0500

    Fixed uploadProcess.php to use /tmp

--- uploadProcess.php
+++ uploadProcess.php
@@ -1,6 +1,6 @@
 <?php
 require('functions.php');
-$uploaddir = '/home/ericboehs/ericboehs.com/addons/'.rand().'/';
+$uploaddir = '/tmp/'.rand().'/';
 $uploadfile = $uploaddir.'versions.zip';
 mkdir($uploaddir);
 if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
@@ -10,7 +10,7 @@ if (move_uploaded_file($_FILES['userfile']['tmp_name'], $uploadfile)) {
     echo "All of your Addons are up to date.";
   }
 } else {
-    echo "Possible file upload attack!\n";
+    echo "Let's not and say we did.\n";
 }
 
 ?>
